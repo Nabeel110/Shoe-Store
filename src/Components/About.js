b168fb8fae768411fc97ba0a25f0fa46c8249d3a
Nabeel Ahmed <nabeelmoolji1@gmail.com>
2020-07-08 11:22:57 +0500

    Fixed Responsiveness

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,51 +5,73 @@ import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import ProfilePic from '../Images/profile_photo.jpg'
 import Grid from '@material-ui/core/Grid';
-import {GitHub} from '@material-ui/icons';
+import { GitHub } from '@material-ui/icons';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-   
-    margin: "100px",
+
+    flexGrow: 1,
+    margin: "100px"
   },
-  paper :{
-      textAlign:"center"
+  paper: {
+    textAlign: "center"
   },
   large: {
     width: theme.spacing(12),
     height: theme.spacing(12),
     // padding:"5px"
   },
+  avatar: {
+    paddingLeft: "20px",
+
+  }
 }));
 
 
 
 const About = () => {
 
-    const classes = useStyles();
-    return (  
-        <div >
-        <Navbar />
-        <div className ={classes.root} style = {{textAlign :"center"}}>
-        <h1 style ={{textAlign: "center", paddingLeft: "12px"}}>ABOUT ME</h1>
-        
-        <Grid container>
-        <Grid  item xs ={12}>
+  const classes = useStyles();
+  return (
+    <div >
+      <Navbar />
+      <div className={classes.root} style={{ textAlign: "center" }}>
+        <h1 style={{ textAlign: "center", paddingLeft: "12px" }}>ABOUT ME</h1>
 
-        <Paper  style ={{backgroundColor: "#c7c5f5",marginLeft: "50px", height:"350px", paddingTop:"3px", paddingBottom:"7px"}} className= {classes.paper} elevation={3}>
-            <h2>Nabeel Ahmed</h2>
-            <div style  ={{paddingLeft: "42%"}}>
-            <Avatar  alt="Remy Sharp" src={ProfilePic} className={classes.large} />
-            </div>
-            <p style ={{fontWeight: "bold", fontSize: "30px", paddingLeft:"5px", paddingRight:"5px"}}><span>If you like the project,give a <span role ="img" aria-label= "Emoji">⭐</span>on my repository</span> </p>
-            <a style ={{listStyle: "none"}} href ="https://github.com/Nabeel110"><GitHub style = {{color:"#BA1D1D"}} fontSize= "large"/><bold>Github</bold></a>
-       
-        </Paper>
+        <Grid container
+          spacing={3}
+          direction="row"
+          alignItems="center"
+          justify="center" zeroMinWidth
+          alignContent="center"
+          wrap="wrap"
+        >
+          <Grid item xs={12} sm={12} md={12} xs-center justify-xs-center>
+
+            <Paper style={{ backgroundColor: "#c7c5f5", height: "390px", paddingTop: "3px", paddingBottom: "20px" }} className={classes.paper} elevation={3}>
+              <h2>Nabeel Ahmed</h2>
+              <Grid container
+                spacing={3}
+                direction="row"
+                alignItems="center"
+                justify="center" zeroMinWidth
+                alignContent="center"
+                wrap="wrap"
+              >
+                <Grid item xs={12} sm={12} md={12} xs-center justify-xs-center></Grid>
+                <Avatar srcSet zeroMinWidth alt="Remy Sharp" src={ProfilePic} className={classes.large} />
+                <Grid />
+              </Grid>
+              <Typography style={{ fontWeight: "bold", fontSize: "30px", padding: "3%" }}><span>"If you liked my project,don't forget to leave a <span role="img" aria-label="Emoji">⭐</span>on my repository."</span> </Typography>
+              <a style={{ listStyle: "none" }} href="https://github.com/Nabeel110"><GitHub style={{ color: "#BA1D1D" }} fontSize="large" /><bold>Github</bold></a>
+
+            </Paper>
+          </Grid>
         </Grid>
-        </Grid>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
- 
+
 export default About;
